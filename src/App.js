e1f6ad33cbec4f766a05a5bf8d384aa10fba0d21
masbag <adityabagast@gmail.com>
2022-05-08 13:25:52 +0700

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavigationBar from "./components/NavigationBar"
+
+function App() {
+  return (
+    <div>
+      <div className='myBG border'>
+        <NavigationBar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
